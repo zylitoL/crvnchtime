f07281d8a9644c6150f38f8c63f4278b721a192a
Alex <68244986+liuhoo@users.noreply.github.com>
2021-05-18 21:14:49 -0700

    Collecting audio volumes

--- optimizer/main.py
+++ optimizer/main.py
@@ -7,14 +7,22 @@ from typing import Iterable, Tuple
 import heuristics
 import numpy as np
 
+
 def audiostream(fname: str) -> np.ndarray:
-    pass
+    filepointer = wave.open(str, "rb")
+    filewave = filepointer.readframes(-1)
+    volume = np.frombuffer(filewave, dtype = 'int16')
+
+    return volume
+
 
 def segments(vols: np.ndarray) -> Iterable[Tuple[int, int]]:
     pass
 
+
 def optimize(fname: str) -> None:
     pass
+
 
 def optimize_files(*fnames: str) -> None:
     pass
