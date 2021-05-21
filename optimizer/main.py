d93cdf7bd7ff58e5ef8081392d9022bc501bdb92
Alex <68244986+liuhoo@users.noreply.github.com>
2021-05-21 10:00:11 -0700

    Added conversion to wav file

--- optimizer/main.py
+++ optimizer/main.py
@@ -6,12 +6,27 @@
 from typing import Iterable, Tuple
 import heuristics
 import numpy as np
+import wave
+import os
 
 
 def audiostream(fname: str) -> np.ndarray:
-    filepointer = wave.open(str, "rb")
+    abs_file_path = os.path.abspath(fname)
+    name = os.path.basename(abs_file_path)
+    name_without_extension = name.split(".")[0]
+
+    print(name)
+    print(abs_file_path)
+
+    print("Obtaining audio stream")
+    os.system("ffmpeg -hide_banner -loglevel warning -i {fpath} -vn -acodec pcm_s16le -ar 44100 -ac 2 ./{out:}.wav".format(
+			fpath=abs_file_path,
+			out=name_without_extension
+	))
+
+    filepointer = wave.open("./{fname:}.wav".format(fname=name_without_extension), "rb")
     filewave = filepointer.readframes(-1)
-    volume = np.frombuffer(filewave, dtype = 'int16')
+    volume = np.frombuffer(filewave, dtype = 'float32')
 
     return volume
 
